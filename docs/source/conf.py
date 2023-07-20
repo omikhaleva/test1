34f159334c32951b36f36e4a94922dd72038918b
OlegKvtnv <139773679+OlegKvtnv@users.noreply.github.com>
2023-07-20 23:12:38 +0300

    edit Ext in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,13 +12,14 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-'sphinx.ext.duration',
-'sphinx.ext.doctest',
-'sphinx.ext.autodoc',
-'sphinx.ext.autosummary',
-'sphinx.ext.intersphinx',
-'sphinx_tabs.tabs',
-'sphinx.ext.imgconverter'
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.imgconverter',
+    'sphinx.ext.viewcode',
+    'sphinx_tabs.tabs',
 ]
 
 intersphinx_mapping = {
