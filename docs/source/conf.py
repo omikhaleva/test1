6bbb2a013a45aaa96db231ca32523c87359ac9ce
OlegKvtnv <139773679+OlegKvtnv@users.noreply.github.com>
2023-07-20 20:03:28 +0300

    Ext tab

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,12 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx_tabs.tabs'
+'sphinx.ext.duration',
+'sphinx.ext.doctest',
+'sphinx.ext.autodoc',
+'sphinx.ext.autosummary',
+'sphinx.ext.intersphinx',
+'sphinx_tabs.tabs'
 ]
 
 intersphinx_mapping = {
